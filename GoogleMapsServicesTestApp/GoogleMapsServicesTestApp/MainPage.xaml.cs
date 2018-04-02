78a5f27496cfcf5b1a0c9f1ea32e3a544dbb9d56
Marcus Vinicius de Lima <marcusvlima@msn.com>
2018-04-02 09:02:14 -0300

    Add project files.

---
+++ GoogleMapsServicesTestApp/GoogleMapsServicesTestApp/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using GoogleMapsServicesClient.NETStandard;
+using GoogleMapsServicesClient.NETStandard.Geocode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace GoogleMapsServicesTestApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            Search.Clicked += (sender, args) =>
+            {
+                GeocodeClient client = new GeocodeClient("https://maps.googleapis.com/maps/api/", "AIzaSyBepGH1lMmdhpQ9CIpq6ip6dep3ACzdFMs", "BR");
+
+                var result = client.GetLocationByAddress(new GeocodeRequestInfo() { CityName = this.CityName.Text, PlaceNumber = int.Parse(this.LocationNumber.Text), PublicPlaceName = this.PublicPlaceName.Text });
+                if (result.Status == GeocodeResult.StatusTypes.OK)
+                {
+                    Result.IsEnabled = true;
+
+                    var mainResult = result.Results.FirstOrDefault();
+                    Result.Text = mainResult.FormattedAddress;
+
+                    var viewMapButton = new Button();
+                    viewMapButton.Text = "View on Map";
+                    viewMapButton.Clicked += (viewMapSender, viewMapArgs) =>
+                    {
+                        Navigation.PushAsync(new MapsPagexaml(mainResult.Geometry.Location));
+                    };
+
+                    ResultsPanel.Children.Add(viewMapButton);
+
+                    foreach (var addressComponent in mainResult.AddressComponents)
+                    {
+                        Label addressNameLabel = new Label();
+                        addressNameLabel.Text = addressComponent.LongName;
+
+                        var typesString = "(";
+                        var types = addressComponent.Types.ToList();
+                        for (int i = 0; i < types.Count; i++)
+                        {
+                            typesString += types[i];
+                            if (i < types.Count - 1)
+                                typesString += " ";
+                        }
+                        typesString += ")";
+                        Label addressTypesLabel = new Label();
+                        addressTypesLabel.Text = typesString;
+
+                        StackLayout stack = new StackLayout();
+                        stack.Orientation = StackOrientation.Horizontal;
+                        stack.Children.Add(addressNameLabel);
+                        stack.Children.Add(addressTypesLabel);
+
+                        ResultsPanel.Children.Add(stack);
+                    }
+                }
+            };
+        }
+	}
+}
---
+++ GoogleMapsServicesTestApp/GoogleMapsServicesTestApp/MapsPagexaml.xaml.cs
@@ -0,0 +1,35 @@
+﻿using GoogleMapsServicesClient.NETStandard.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace GoogleMapsServicesTestApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MapsPagexaml : ContentPage
+	{
+		public MapsPagexaml (Location location)
+		{
+			InitializeComponent ();
+            MyMap.MoveToRegion(
+                MapSpan.FromCenterAndRadius(
+                    new Position(location.Lat, location.Lng), Distance.FromMiles(1)));
+
+            var position = new Position(location.Lat, location.Lng); // Latitude, Longitude
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = position,
+                Label = "My Place",
+                Address = "custom detail info"
+            };
+            MyMap.Pins.Add(pin);
+        }
+	}
+}
